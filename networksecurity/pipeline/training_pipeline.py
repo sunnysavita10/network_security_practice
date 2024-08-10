20c5d496803497d822c7f2a6cae6b6dfb6440192
sunnysavita10 <engsunny3365@gmail.com>
2024-08-11 02:03:05 +0530

    code updated of the entire pipeline

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -107,22 +107,27 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    def model_evaluation(self):
-        """_summary_
-        """
+    def start_model_evaluation(self,data_validation_artifact:DataValidationArtifact,
+                                 model_trainer_artifact:ModelTrainerArtifact,
+                                ):
         try:
-            pass
-        except Exception as e:
-            raise NetworkSecurityException(e, sys)
+            model_eval_config = ModelEvaluationConfig(self.training_pipeline_config)
+            model_eval = ModelEvaluation(model_eval_config, data_validation_artifact, model_trainer_artifact)
+            model_eval_artifact = model_eval.initiate_model_evaluation()
+            return model_eval_artifact
+        except  Exception as e:
+            raise  NetworkSecurityException(e,sys)
     
     def model_pusher(self):
         """_summary_
         """
         try:
-            pass
-        except Exception as e:
-            raise NetworkSecurityException(e, sys)
-        
+            model_pusher_config = ModelPusherConfig(training_pipeline_config=self.training_pipeline_config)
+            model_pusher = ModelPusher(model_pusher_config, model_eval_artifact)
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except  Exception as e:
+            raise  NetworkSecurityException(e,sys)
     def run_pipeline(self):
         try:
             TrainingPipeline.is_pipeline_running = True
@@ -131,8 +136,17 @@ class TrainingPipeline:
             data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact: ModelTrainerArtifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
-            #self.model_evaluation()
-            #self.model_pusher()
+            model_eval_artifact = self.start_model_evaluation(data_validation_artifact, model_trainer_artifact)
+            if not model_eval_artifact.is_model_accepted:
+                raise Exception("Trained model is not better than the best model")
+            model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
+            
+            
+            TrainingPipeline.is_pipeline_running=False
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
             
         except Exception as e:
+            self.sync_artifact_dir_to_s3()
+            TrainingPipeline.is_pipeline_running=False
             raise NetworkSecurityException(e, sys)
