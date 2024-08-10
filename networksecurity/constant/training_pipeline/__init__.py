e7c4dfc1d04b4021af7d938e9cd103a751beacab
sunnysavita10 <engsunny3365@gmail.com>
2024-08-10 22:54:12 +0530

    data validation component written

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -13,7 +13,7 @@ TEST_FILE_NAME: str = "test.csv"
 
 PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 MODEL_FILE_NAME = "model.pkl"
-SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 SCHEMA_DROP_COLS = "drop_columns"
 
 
