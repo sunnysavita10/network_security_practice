05363f5edf22b7322e94e87dae515084e5ca9547
sunnysavita10 <engsunny3365@gmail.com>
2024-08-11 00:52:31 +0530

    data transformation code update

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -1,14 +1,12 @@
 import sys
-
+import os
 import numpy as np
 import pandas as pd
-from imblearn.combine import SMOTETomek
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import RobustScaler
+from sklearn.impute import KNNImputer
 from sklearn.pipeline import Pipeline
 
-
 from networksecurity.constant.training_pipeline import TARGET_COLUMN
+from networksecurity.constant.training_pipeline import DATA_TRANSFORMATION_IMPUTER_PARAMS
 from networksecurity.entity.artifact_entity import (
     DataTransformationArtifact,
     DataValidationArtifact,
@@ -31,11 +29,11 @@ class DataTransformation:
         :param data_transformation_config: configuration for data transformation
         """
         try:
-            self.data_validation_artifact = data_validation_artifact
-            self.data_transformation_config = data_transformation_config
+            self.data_validation_artifact: DataValidationArtifact = data_validation_artifact
+            self.data_transformation_config: DataTransformationConfig = data_transformation_config
 
         except Exception as e:
-            raise SensorException(e, sys)
+            raise NetworkSecurityException(e, sys)
 
 
     @staticmethod
@@ -43,61 +41,77 @@ class DataTransformation:
         try:
             return pd.read_csv(file_path)
         except Exception as e:
-            raise SensorException(e, sys)
+            raise NetworkSecurityException(e, sys)
 
 
-    @classmethod
-    def get_data_transformer_object(cls)->Pipeline:
+    def get_data_transformer_object(cls) -> Pipeline:
+        """
+        It initialises a KNNImputer object with the parameters specified in the training_pipeline.py file
+        and returns a Pipeline object with the KNNImputer object as the first step.
+
+        Args:
+          cls: DataTransformation
+
+        Returns:
+          A Pipeline object
+        """
+        logging.info(
+            "Entered get_data_transformer_object method of DataTransformation class"
+        )
+
         try:
-            robust_scaler = RobustScaler()
-            simple_imputer = SimpleImputer(strategy="constant", fill_value=0)
-            preprocessor = Pipeline(
-                steps=[
-                    ("Imputer", simple_imputer), #replace missing values with zero
-                    ("RobustScaler", robust_scaler) #keep every feature in same range and handle outlier
-                    ]
+            imputer: KNNImputer = KNNImputer(
+                **DATA_TRANSFORMATION_IMPUTER_PARAMS
             )
-            
+
+            logging.info(
+                f"Initialise KNNImputer with {DATA_TRANSFORMATION_IMPUTER_PARAMS}"
+            )
+
+            preprocessor: Pipeline = Pipeline([("imputer", imputer)])
+
+            logging.info(
+                "Exited get_data_transformer_object method of DataTransformation class"
+            )
+
             return preprocessor
 
         except Exception as e:
-            raise SensorException(e, sys) from e
+            raise NetworkSecurityException(e, sys)
+
 
     
     def initiate_data_transformation(self,) -> DataTransformationArtifact:
+        logging.info("Entered initiate_data_transformation method of DataTransformation class")
+
         try:
+            logging.info("Starting data transformation")
+            
+            os.makedirs(self.data_transformation_config.transformed_data_dir, exist_ok=True)
             
             train_df = DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
             test_df = DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
             preprocessor = self.get_data_transformer_object()
-
+            
+            logging.info("Got the preprocessor object")
 
             #training dataframe
             input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN], axis=1)
             target_feature_train_df = train_df[TARGET_COLUMN]
-            target_feature_train_df = target_feature_train_df.replace( TargetValueMapping().to_dict())
+            target_feature_train_df = target_feature_train_df.replace(-1, 0)
 
             #testing dataframe
             input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN], axis=1)
             target_feature_test_df = test_df[TARGET_COLUMN]
-            target_feature_test_df = target_feature_test_df.replace(TargetValueMapping().to_dict())
+            target_feature_test_df = target_feature_test_df.replace(-1, 0)
 
             preprocessor_object = preprocessor.fit(input_feature_train_df)
             transformed_input_train_feature = preprocessor_object.transform(input_feature_train_df)
             transformed_input_test_feature =preprocessor_object.transform(input_feature_test_df)
+            
 
-            smt = SMOTETomek(sampling_strategy="minority")
-
-            input_feature_train_final, target_feature_train_final = smt.fit_resample(
-                transformed_input_train_feature, target_feature_train_df
-            )
-
-            input_feature_test_final, target_feature_test_final = smt.fit_resample(
-                transformed_input_test_feature, target_feature_test_df
-            )
-
-            train_arr = np.c_[input_feature_train_final, np.array(target_feature_train_final) ]
-            test_arr = np.c_[ input_feature_test_final, np.array(target_feature_test_final) ]
+            train_arr = np.c_[transformed_input_train_feature, np.array(target_feature_train_df) ]
+            test_arr = np.c_[ transformed_input_test_feature, np.array(target_feature_test_df) ]
 
             #save numpy array data
             save_numpy_array_data( self.data_transformation_config.transformed_train_file_path, array=train_arr, )
@@ -114,5 +128,5 @@ class DataTransformation:
             logging.info(f"Data transformation artifact: {data_transformation_artifact}")
             return data_transformation_artifact
         except Exception as e:
-            raise SensorException(e, sys) from e
+            raise NetworkSecurityException(e, sys) from e
 
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -1,9 +1,10 @@
 import os
+import numpy as np
 #from networksecurity.constant.s3_bucket import TRAINING_BUCKET_NAME
 
 SAVED_MODEL_DIR =os.path.join("saved_models")
 # defining common constant variable for training pipeline
-TARGET_COLUMN = "class"
+TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifacts"
 FILE_NAME: str = "NetworkData.csv"
@@ -44,6 +45,16 @@ Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
+
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
 
 """
 Model Trainer ralated constant start with MODE TRAINER VAR NAME
